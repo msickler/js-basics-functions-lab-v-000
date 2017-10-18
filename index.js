ae6303cae32dd933adedd0eab5f258555aed694c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-18 18:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+  Math.abs(distance - 42)
+}
+ 
+function distanceFromHqInFeet () {
+  distanceFromHqInBlocks();
+  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function
+}
