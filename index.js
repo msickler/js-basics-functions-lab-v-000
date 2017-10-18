9df2118037fe8708a5907f2294b4aa3c0745bd16
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-18 18:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function distanceTravelledInFeet(start, end) {
 }
 
 function calculatesFarePrice(start, end) {
-  distanceTravelledInFeet(start, end);
+  const distance = distanceTravelledInFeet(start, end);
   switch(true){
      case distance < 400:
        return 0;
